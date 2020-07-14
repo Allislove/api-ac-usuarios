ef904235d1a25eb549c7e87cab63a3c86d613b9e
Andres <xandrewssx970@outlook.es>
2020-07-14 15:54:43 -0500

    Get status ok

--- src/App.js
+++ src/App.js
@@ -1,13 +1,50 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Form from './components/form';
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      usuarios: {}
+      // Creo un objeto vacio para meter los usuarios de la API
+    }
+  }
+
+    //Se ejecuta cuando se renderiza este componente
+    componentDidMount(){
+        let proxyUrl = 'https://cors-anywhere.herokuapp.com/',
+            targetUrl = 'https://academlo-api-users.herokuapp.com/users';
+        fetch(proxyUrl + targetUrl)
+            .then(blob => blob.json())
+            .then(data => {
+                console.table(data); // lo imprimo en una tabla
+                this.setState({usuarios: data});
+                console.log(data)
+                return data;
+            })
+            .catch(e => {
+                console.log(e);
+                return e;
+            });
+    }
+
+  render() {
+    return (
+        <div className="App">
+            <h1> Hello World </h1>
+
+
+            <p> Usuario: {this.state.usuarios.name} </p>
+            {/*console.log({this.state.usuarios.data});*/}
+
+            {/*console.log({this.state.usuarios.name});*/}
+            <Form />
+        </div>
+    );
+
+  }
+
 }
 
 export default App;
---
+++ src/components/form.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default class Form extends React.Component {
+    render() {
+        return(
+            <div>
+                <input type="text" placeholder="Ingresa el dato"/>
+                <input type="submit"   />
+
+            </div>
+        );
+    }
+}
